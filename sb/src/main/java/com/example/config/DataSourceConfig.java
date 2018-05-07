56add2c6a65fd27204c32ca1ccc366dcde65ba29
sylarjyd <920221738@qq.com>
2018-05-07 15:23:42 +0800

    20180507

---
+++ sb/src/main/java/com/example/config/DataSourceConfig.java
@@ -0,0 +1,42 @@
+package com.example.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+@PropertySource(value= {"classpath:application.properties"},encoding="utf-8")
+public class DataSourceConfig {
+
+	@Value("${spring.datasource.url}")
+	private String url;
+	
+	@Value("${spring.datasource.username}")
+	private String username;
+	
+	@Value("${spring.datasource.password}")
+	private String password;
+	
+	@Value("${spring.datasource.driver-class-name}")
+	private String driverClassName;
+	
+    @Bean(name = "dataSource")
+    @Qualifier("dataSource")
+    public DataSource dataSource() {
+         return DataSourceBuilder.create().build();
+         
+    }
+	
+    @Bean(name = "jdbcTemplate")
+    public JdbcTemplate jdbcTemplate(
+            @Qualifier("dataSource") DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+	
+}
